470151dde626ae9f60cb8a7ac0e97a3edd4f4711
murkney <36377174+murkney@users.noreply.github.com>
2022-12-01 17:26:00 +0100

    fixed slideshow errors, more customize will come

--- js/modules/slideshow.js
+++ js/modules/slideshow.js
@@ -1,31 +1,45 @@
-export default function Slideshow() {
-	// Slides index
-	let slideIndex = 1;
-	showSlides(slideIndex);
+// Slides index
+let slideIndex = 1;
+showSlides(slideIndex);
 
-	// Next/previous controls
-	function plusSlides(number) {
-	showSlides(slideIndex += number);
-	};
-
-	// Thumbnail image controls
-	function currentSlide(number) {
-	showSlides(slideIndex = number);
-	};
-
-	function showSlides(number) {
-	let index;
-	let slides = document.getElementsByClassName("slideshow__images");
-	let dots = document.getElementsByClassName("dot");
-	if (number > slides.length) {slideIndex = 1}
-	if (number < 1) {slideIndex = slides.length}
-	for (index = 0; index < slides.length; index += 1) {
-		slides[index].style.display = "none";
-	};
-	for (index = 0; index < dots.length; index += 1) {
-		dots[index].className = dots[index].className.replace("active", "");
-	};
-	slides[slideIndex-1].style.display = " block";
-	dots[slideIndex-1].className += " active";
-	};
+// Next/previous controls
+function plusSlides(number) {
+showSlides(slideIndex += number);
 };
+
+// Thumbnail image controls
+function currentSlide(number) {
+showSlides(slideIndex = number);
+};
+
+function showSlides(number) {
+let index;
+let slides = document.getElementsByClassName("slideshow__images");
+let dots = document.getElementsByClassName("dot");
+if (number > slides.length) {slideIndex = 1}
+if (number < 1) {slideIndex = slides.length}
+for (index = 0; index < slides.length; index += 1) {
+	slides[index].style.display = "none";
+};
+for (index = 0; index < dots.length; index += 1) {
+	dots[index].className = dots[index].className.replace("active", "");
+};
+slides[slideIndex-1].style.display = " block";
+dots[slideIndex-1].className += " active";
+};
+
+// // Navigate with left & rigth keys
+// // const prevSlide = document.querySelector('.prev');
+// // const nextSlide = document.querySelector('.next');
+
+
+// // window.addEventListeners('keyup', prevSlide);
+// // window.addEventListeners('keydown', nextSlide);
+
+// // function prevSlide(number) {
+// //   showSlides(slideIndex += number);
+// // }
+
+// // function nextSlide(number) {
+// //   showSlides(slideIndex += number);
+// // }
