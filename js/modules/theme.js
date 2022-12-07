cba2ed8a989c78173c0a4fc7ed5c0f8693e5746a
murkney <36377174+murkney@users.noreply.github.com>
2022-12-07 21:04:04 +0100

    dark mode toggle locally stored

--- js/modules/theme.js
+++ js/modules/theme.js
@@ -1,15 +1,29 @@
 export default function Theme() {
-	const icon = document.getElementById("icon");
 	
+	// Variables
+	let darkMode = localStorage.getItem("dark-mode");
+
+	// Query-Selector
+	const icon = document.querySelector(".icon");
+	
+	// Event-Listeners
 	icon.addEventListener('click', toggleTheme);
 	
+	// Event-Handlers
 	function toggleTheme() {
 		document.body.classList.toggle("dark-mode");
 		if (document.body.classList.contains("dark-mode")) {
 			icon.src = "../assets/icons/sun.png";
+			localStorage.setItem("dark-mode", "enabled");
 		} else {
 			icon.src = "../assets/icons/moon.png";
+			localStorage.setItem("dark-mode", "disabled");
 		}
+	};
+
+	// If dark mode is enabled in local storage, dark mode variables will be active.
+	if (darkMode === "enabled") {
+		toggleTheme();
 	};
 };
 
