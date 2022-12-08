be168cb61aa682b7d25dd3b927e270da896444a6
murkney <36377174+murkney@users.noreply.github.com>
2022-12-09 00:45:52 +0100

    removed unused items

--- js/main.js
+++ js/main.js
@@ -1,12 +1,7 @@
-import Collapsible from './modules/collapsible.js';
 import Header from './modules/header.js';
 import Theme from './modules/theme.js';
 import Tasks from './modules/task-list.js';
 import Slideshow from './modules/slideshow.js';
-
-for (const collapsibleNode of document.querySelectorAll('.collapsible')) {
-	Collapsible(collapsibleNode);
-}
 
 Header();
 Theme();
